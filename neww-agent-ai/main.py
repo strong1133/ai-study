4fec57ecd759856767bde3fceb6fd66e382daa23
strong3879 <strong3879@gmail.com>
2025-09-26 09:17:22 +0900

    [노마드강의] crew ai tool 사용 기본

--- neww-agent-ai/main.py
+++ neww-agent-ai/main.py
@@ -1,39 +1,40 @@
+from re import S
 import dotenv
+
 dotenv.load_dotenv()
 
 from crewai import Crew, Agent, Task
 from crewai.project import CrewBase, agent, task, crew
-
-
+from tools import count_letters
 
 
 @CrewBase
 class TranslatorCrew:
-    
 
     @agent
     def translator_agent(self):
+        return Agent(config=self.agents_config["translator_agent"])
+
+    @agent
+    def counter_agent(self):
         return Agent(
-            config = self.agents_config["translator_agent"]
+            config=self.agents_config["counter_agent"],
+            tools=[count_letters],
         )
 
     @task
     def translate_task(self):
-        return Task(
-            config= self.tasks_config["translate_task"]
-        )
+        return Task(config=self.tasks_config["translate_task"])
+
+    @task
+    def count_task(self):
+        return Task(config=self.tasks_config["count_task"])
 
     @crew
     def assemble_crew(self):
-        return Crew(
-            agents=self.agents,
-            tasks=self.tasks,
-            verbose=True
-        )
+        return Crew(agents=self.agents, tasks=self.tasks, verbose=True)
 
 
 TranslatorCrew().assemble_crew().kickoff(
-    inputs={
-        "sentence":"Hello, I'm SeokJin, my job is developer and I like ai coding "
-    }
-) 
+    inputs={"sentence": "Hello, I'm SeokJin, my job is developer and I like ai coding "}
+)
